1360dc0ff4b931e6c9bce885a268c644d161f579
manhnv <manhnv130100@gmail.com>
2023-05-23 21:07:36 +0700

    fix init

--- db/migrate/20230522070756_devise_create_users.rb
+++ db/migrate/20230522070756_devise_create_users.rb
@@ -4,8 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string  :name,              null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :avatar
+      t.boolean :is_admin
 
       ## Recoverable
       t.string   :reset_password_token
--- db/migrate/20230522071311_add_fields_to_u_sers.rb
+++
@@ -1,4 +0,0 @@
-class AddFieldsToUSers < ActiveRecord::Migration[7.0]
-  def change
-  end
-end
